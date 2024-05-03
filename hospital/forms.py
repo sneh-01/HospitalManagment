e3b26987ad5dd11c56c5d88c797cd8439eace2f2
sneh-01 <122522055+sneh-01@users.noreply.github.com>
2024-05-03 22:57:01 +0530

    commit

--- hospital/forms.py
+++ hospital/forms.py
@@ -64,7 +64,7 @@ class PatientAppointmentForm(forms.ModelForm):
 
 
 #for contact us page
-class ContactusForm(forms.Form):
+class ContactusForm(forms.Form):                                                                                                                   
     Name = forms.CharField(max_length=30)
     Email = forms.EmailField()
     Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
